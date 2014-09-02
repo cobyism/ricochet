326ddab850f09708195cac3ddc9c84834e8afda9
Coby Chapple <coby@cobychapple.com>
2014-09-02 21:14:13 +0100

    make ricochet executable via PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,6 +1,6 @@
 # Customize $PATH to your needs...
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
-export PATH="$DOTFILES/bin:$PATH"
+export PATH="$RICOCHET/bin:$PATH"
 export PATH="$HOME/scripts/bin:$PATH"
 export PATH="$HOME/node_modules/.bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
