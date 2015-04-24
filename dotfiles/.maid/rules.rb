52dff4fd3574a614bc1d892740b711af2a641ef6
Coby Chapple <coby@cobychapple.com>
2015-04-24 14:37:23 +0100

    fix up maid rules to not need to include dimensions gem

--- dotfiles/.maid/rules.rb
+++ dotfiles/.maid/rules.rb
@@ -1,12 +1,10 @@
-require 'dimensions'
-
 Maid.rules do
   rule 'Sort Camera Uploads in Dropbox into appropriate folders' do
     camera_uploads = "~/Dropbox (Personal)/Camera Uploads"
     files(File.join(camera_uploads, "*")).each do |path|
       if media_type(path) == "image"
         filename = File.basename(path)
-        if [[640, 1136], [1136, 640]].include? Dimensions.dimensions(path)
+        if [[640, 1136], [1136, 640]].include? dimensions_px(path)
           new_path = File.join(camera_uploads, "Screenshots", filename)
           rename(path, new_path)
         else
@@ -17,8 +15,13 @@ Maid.rules do
         end
       end
 
-      # if media_type(path) == "video"
-      # end
+      if media_type(path) == "video" || File.extname(path) == ".AAE"
+        filename = File.basename(path)
+        year_folder  = modified_at(path).year.to_s
+        month_folder = modified_at(path).strftime("%m-%b")
+        new_path     = File.join(camera_uploads, year_folder, month_folder, "movies", filename)
+        rename(path, new_path)
+      end
     end
   end
 end
