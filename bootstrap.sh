ef071cef63a5494ce4205912ba20533d1459847b
Coby Chapple <coby@cobychapple.com>
2015-09-12 17:52:50 +0100

    take a stab at some updates with a bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
-brew install ansible
+echo ""
+echo "WELCOME TO RICOCHET!"
+echo ""
+
+if [ ! command -v brew >/dev/null 2>&1 ] && [ ! command -v brew cask >/dev/null 2>&1 ]; then
+  echo "Let’s get you set up first by running strap…"
+  echo ""
+  if [ ! -f ~/Downloads/strap.sh ]; then
+    echo "It doesn’t look like you’ve downloaded strap.sh yet. Let me open that in a browser for you."
+    open https://osx-strap.herokuapp.com/strap.sh
+    echo "Run this again once you’ve got strap.sh in ~/Downloads :)"
+  else
+    echo "Running strap.sh…"
+    echo ""
+    source ~/Downloads/strap.sh
+    echo ""
+    echo "Successfully finished running strap. Now, onto the good stuff!"
+    echo ""
+  fi
+else
+  echo "Looks like this isn’t you’re first rodeo! Skipping straight to the good stuff."
+  echo ""
+fi
+
+if ! command -v ansible-playbook >/dev/null 2>&1; then
+  echo "Looks like we ain’t got any ansible yet. Let’s do something about that, aye?"
+  echo ""
+  brew install ansible
+  echo "Ansibled up."
+  echo ""
+fi
+
+echo "Running Ricochet…"
+echo ""
 export RICOCHET=/Users/cobyism/code/ricochet
-./bin/ricochet brews
-./bin/ricochet casks
-./bin/ricochet all
+$RICOCHET/bin/ricochet all
+
+echo "Bing! All done :)"
