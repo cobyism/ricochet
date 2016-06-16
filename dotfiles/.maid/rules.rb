8f5befc01181b0b268436af1f7d975953f9c8db4
Coby Chapple <coby@cobychapple.com>
2016-06-16 13:48:09 +0100

    install maid daemon as launch agent

--- dotfiles/.maid/rules.rb
+++ dotfiles/.maid/rules.rb
@@ -1,26 +1,34 @@
 Maid.rules do
-  rule 'Sort Camera Uploads in Dropbox into appropriate folders' do
-    camera_uploads = "~/Dropbox (Personal)/Camera Uploads"
-    files(File.join(camera_uploads, "*")).each do |path|
-      if media_type(path) == "image"
-        filename = File.basename(path)
-        if [[640, 1136], [1136, 640]].include? dimensions_px(path)
-          new_path = File.join(camera_uploads, "Screenshots", filename)
-          rename(path, new_path)
-        else
+  repeat '15m' do
+    rule 'Sort Dropbox Camera Uploads into appropriate folders' do
+      puts "hello from #{Time.now}!"
+      camera_uploads = "~/Dropbox (Personal)/Camera Uploads"
+      files(File.join(camera_uploads, "*")).each do |path|
+        if media_type(path) == "image"
+          filename = File.basename(path)
+          if [[640, 1136], [1136, 640]].include? dimensions_px(path)
+            new_path = File.join(camera_uploads, "Screenshots", filename)
+            rename(path, new_path)
+          else
+            year_folder  = modified_at(path).year.to_s
+            month_folder = modified_at(path).strftime("%m-%b")
+            new_path     = File.join(camera_uploads, year_folder, month_folder, filename)
+            rename(path, new_path)
+          end
+        end
+
+        also_video = [
+          ".aae",
+          ".mp4" # mp4 media_type comes through as 'application'?
+        ]
+
+        if media_type(path) == "video" || also_video.include?(File.extname(path).downcase)
+          filename = File.basename(path)
           year_folder  = modified_at(path).year.to_s
           month_folder = modified_at(path).strftime("%m-%b")
-          new_path     = File.join(camera_uploads, year_folder, month_folder, filename)
+          new_path     = File.join(camera_uploads, year_folder, month_folder, "movies", filename)
           rename(path, new_path)
         end
-      end
-
-      if media_type(path) == "video" || File.extname(path) == ".AAE"
-        filename = File.basename(path)
-        year_folder  = modified_at(path).year.to_s
-        month_folder = modified_at(path).strftime("%m-%b")
-        new_path     = File.join(camera_uploads, year_folder, month_folder, "movies", filename)
-        rename(path, new_path)
       end
     end
   end
